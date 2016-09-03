d988612d751c7dbf18d7901f430763f5bc3bc217
Dmitry <basher9696@gmail.com>
2016-09-03 16:27:35 +0300

    some changes

--- app/overrides/update_header.rb
+++ app/overrides/update_header.rb
@@ -12,6 +12,8 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :insert_after => "body",
                      :partial => "shared/footer")
  
+
+
 Deface::Override.new(:virtual_path => "spree/shared/_nav_bar", 
                   :name => "change header", 
                   :insert_after => "nav#top-nav-bar") do '    
@@ -27,31 +29,6 @@ Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
                   end
 
 
-
-
 Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar", 
                   :name => "delete main_nav_bar", 
                   :remove => "div#main-nav-bar")
-
-
-
-
-Deface::Override.new(:virtual_path => "spree/shared/_nav_bar", 
-                   :name => "change header", 
-                   :insert_after => "nav#top-nav-bar") do  
-						'<ul class="nav navbar-nav navbar-right" data-hook>
-      						<li id="link-to-cart" data-hook>
-        					<noscript>
-          						<%= link_to Spree.t(:cart), "/cart" %>
-        					</noscript>
-        						&nbsp;
-      						</li>
-      							<script>Spree.fetch_cart()</script>
-    					</ul>' 
-end
-
-
-Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar", 
-                   :name => "delete main_nav_bar", 
-                   :remove => "div#main-nav-bar")                                  
-
