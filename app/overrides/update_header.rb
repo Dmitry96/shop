674fb2bc3daddb6376f7beef061a2767406dda15
Dmitry <basher9696@gmail.com>
2016-09-03 12:45:18 +0300

    shop on pg

--- app/overrides/update_header.rb
+++ app/overrides/update_header.rb
@@ -3,29 +3,94 @@ Deface::Override.new(:virtual_path => "spree/shared/_header",
                      :name => "change header add h3 Store ", 
                      :replace => "figure#logo") do "<a id='logo' class='navbar-brand' href='/'> Store </a>" end
 
-Deface::Override.new(:virtual_path => "spree/shared/_taxonomies", 
-                     :name => "change taxon partioal", 
-                     :replace => "h4.taxonomy-root") do 
-                      "<a>Hello</a>"
-                      end
+
+
+
 # <%= taxons_tree(taxonomy.root, @taxon, max_level) %>
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
                      :name => "add footer", 
                      :insert_after => "body") do 
-                      "<footer id='footer'>
-                        <div class='container'>
-                          <p class=''>Place sticky footer content here.</p>
-                        </div>
-                      </footer>"
-                      end
+												"<br/>
+												<footer id='footer'>
+													<div id='background'>
+
+													<div class='container'>
+															
+													<div class='col-lg-3 col-md-3 col-sm-6'>
+										            <div class='column'>
+										            	<ul>
+									                	<li><div class='footer_title'>Информация</div></li>
+									                    
+								                    <li><%= link_to 'Политика конфиденциальности', main_app.policy_path %></li>
+								                    <li><%= link_to 'Пользовательское соглашение', main_app.user_agreement_path%></li>
+                                    <li><%= link_to 'О нас'                      , main_app.about_path %></li>
+									                </ul>
+										            </div>
+										        	</div>
+
+										        	<div class='col-lg-3 col-md-3 col-sm-6'>
+										            <div class='column'>
+										            	<ul>
+									                	<li><div class='footer_title'>Обслуживание клиентов</div></li>
+								                    <li><a>Телефон: Your phone number</a></li>
+								                    <li><a href='mailto:example@gmail.com'>Email: example@gmail.com</a></li>
+									                </ul>
+										            </div>
+										        	</div>
 
 
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
-                    :name => "add footer", 
-                    :insert_after => "body") do 
-          '<footer class="footer">
-            <div class="container">
-                <p class="text-muted">Place sticky footer content here.</p>
-            </div>
-          </footer>'
-          end
+															<div class='col-lg-3 col-md-3 col-sm-6'>
+										            <div class='column'>
+										            	<ul class='social'>
+									                	<li><div class='footer_title'>Социальные сети</div></li>
+								                    <li><a href='/' target='_blank'>Вконтакте</a></li>
+									                </ul>
+									            	</div>
+										        	</div>
+										        	
+														</div>
+													</div>
+												</footer>"
+end
+ 
+Deface::Override.new(:virtual_path => "spree/shared/_nav_bar", 
+                  :name => "change header", 
+                  :insert_after => "nav#top-nav-bar") do '    
+                        <ul class="nav navbar-nav navbar-right" data-hook>
+                             <li id="link-to-cart" data-hook>
+                           <noscript>
+                                 <%= link_to Spree.t(:cart), "/cart" %>
+                           </noscript>
+                               &nbsp;
+                             </li>
+                                 <script>Spree.fetch_cart()</script>
+                       </ul>' 
+                   end
+
+
+Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar", 
+                  :name => "delete main_nav_bar", 
+                  :remove => "div#main-nav-bar")
+
+
+
+
+Deface::Override.new(:virtual_path => "spree/shared/_nav_bar", 
+                   :name => "change header", 
+                   :insert_after => "nav#top-nav-bar") do  
+						'<ul class="nav navbar-nav navbar-right" data-hook>
+      						<li id="link-to-cart" data-hook>
+        					<noscript>
+          						<%= link_to Spree.t(:cart), "/cart" %>
+        					</noscript>
+        						&nbsp;
+      						</li>
+      							<script>Spree.fetch_cart()</script>
+    					</ul>' 
+end
+
+
+Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar", 
+                   :name => "delete main_nav_bar", 
+                   :remove => "div#main-nav-bar")                                  
+
